6a262397b32c36ed2a1e48ebaf5cf61c58e78b19
kohijelly <cassie.zhong@outlook.de>
2022-09-18 11:31:12 +0200

    2022. 9.17
    ....

---
+++ 2022_09_Übungsblatt.py
@@ -0,0 +1,110 @@
+#Erinnerungen 
+i = 1
+while i <= 100:
+    print (i)
+    i = i + + 1
+
+
+#liste = []
+
+#list.append(1)
+#print (liste)
+
+i = 1
+list = []
+list.append(i)
+while i <= 10 :
+    print(i)
+    i = i + 1
+   
+print(list)
+
+i = 75
+list = []
+list.append(i)
+while i <= 125 :
+    print(i)
+    i = i + 1
+   
+print(list)
+
+# -35 bis 35
+i = -35
+list = []
+list.append(i)
+while i <= 35 :
+    print(i)
+    i = i + 1
+print(list)
+#0 bis 40 in 2er schritten
+i = 0
+list = []
+list.append(i)
+while i <= 40 :
+    print (i)
+    i = i + 2
+print(list)
+# 30 bis 90 durch 6 teilbar
+i = 30 
+list = []
+list.append(i)
+while i <= 90:
+    print(i)
+    i = i + 6
+print(list)
+# -120 bis 120 durch 12 teilbar 
+i = -120
+list = []
+list.append(i)
+while i < 120 :
+    print(i)
+    i = i + 12
+print(list)
+
+# 10 bis 1
+i = 10 
+list = []
+list.append(i)
+while i >= 1 :
+    print(i)
+    i = i - 1
+print(list)
+# 350 bis 210
+i = 350
+list = []
+list.append(i)
+while i >= 210 :
+    print(i)
+    i = i - 1
+print(list)
+
+# 0 bis -25
+
+# -90 bis - 140
+
+# 88 bis 44 durch 4 telbar
+
+# 0 bis 100 durch 3 und 5 teilbahr 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
