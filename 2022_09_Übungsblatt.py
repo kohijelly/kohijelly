edc6b60362b75bffe687f35bee51774b2c0526e3
kohijelly <cassie.zhong@outlook.de>
2022-09-25 11:31:01 +0200

    Update 2022_09_Übungsblatt.py

--- 2022_09_Übungsblatt.py
+++ 2022_09_Übungsblatt.py
@@ -10,93 +10,207 @@ while i <= 100:
 #list.append(1)
 #print (liste)
 
-i = 1
+
 list = []
-list.append(i)
+i = 1
 while i <= 10 :
-    print(i)
-    i = i + 1
-   
+    list.append(i)
+    i = i + 1  
 print(list)
 
-i = 75
+
 list = []
-list.append(i)
+i = 75
 while i <= 125 :
-    print(i)
-    i = i + 1
-   
+    list.append(i)
+    i = i + 1 
 print(list)
 
 # -35 bis 35
-i = -35
+
 list = []
-list.append(i)
+i = -35
 while i <= 35 :
-    print(i)
+    list.append(i)
     i = i + 1
 print(list)
 #0 bis 40 in 2er schritten
-i = 0
 list = []
-list.append(i)
+i = 0 
 while i <= 40 :
-    print (i)
+    list.append(i)
     i = i + 2
 print(list)
 # 30 bis 90 durch 6 teilbar
-i = 30 
+
 list = []
-list.append(i)
+i = 30
 while i <= 90:
-    print(i)
+    list.append(i)
     i = i + 6
 print(list)
 # -120 bis 120 durch 12 teilbar 
-i = -120
+
 list = []
-list.append(i)
-while i < 120 :
-    print(i)
+i = -120
+while i <= 120 :
+    list.append(i)
     i = i + 12
 print(list)
 
 # 10 bis 1
-i = 10 
+
 list = []
-list.append(i)
+i = 10 
 while i >= 1 :
-    print(i)
+    list.append(i)
     i = i - 1
 print(list)
 # 350 bis 210
-i = 350
-list = []
-list.append(i)
+
+list = [ ]
+i = 350 
+
 while i >= 210 :
-    print(i)
+    
+    list.append(i)
+    
     i = i - 1
 print(list)
 
 # 0 bis -25
-i= 0
-while i >= -25
-    print(i)
+
+list = []
+i = 0
+while i >= -25:
+    
+    list.append(i)
     i = i -1
+print(list)
 # -90 bis - 140
-i= -90
-while i >= -140
-    print(i)
-    i = i -1
-# 88 bis 44 durch 4 telbar
 
+list = []
+i = -90
+while i >= -140:
+    list.append(i)
+    i = i - 2
+print(list)
+
+# 88 bis 44 durch 4 teilbar
+list = []
+i = 88
+while i >= 44:
+    list.append(i)
+    i = i -4
+    print(list)
 # 0 bis 100 durch 3 und 5 teilbahr 
+list=[]
+i = 0
+while i <= 100:
+    list.append(i)
+    i = i + 15
+print(list)
+
+# 0 bis 100 durch 2 und 4
+list = [] 
+i = 0
+while i <= 100:
+    list.append(i)
+    i = i + 4
+print(list)
+
+# 0 bis 100 quaadratzahlen 
+list = []
+i = 0 
+while i <= 10:
+    list.append(i * i)
+    i = i + 1
+print(list)
+
+# 1 bis 1024 alle zweierpotenzen ( 1 2 4 8 16 32 .... 512 1024)
+list = []
+i = 1
+while i <= 1024:
+    list.append(i)
+    i = i * 2
+print(list)
+#alle zahlen von untere grnze bis 100 wenn utere grenze veräfert e´wird soll sie sich anpassen ntere grenze = 1 
+untere_grenze = 1 
+list = []
+i = untere_grenze 
+while i <= 100:
+    list.append(i)
+    i = i + 1 
+print(list)
+
+#alle zahlen von 1 bis oberste grenze = 100 
+obere_grenze = 100
+list = []
+i = 1
+while i <= obere_grenze:
+    list.append(i)
+    i = i + 1 
+print(list)
+
+#untere grenze = 1 obere grenze = 100
+untere_grenze = 1
+obere_grenze = 100
+list = []
+i = untere_grenze 
+while i <= obere_grenze:
+    list.append(i)
+    i = i + 1
+print(list)
+
+#teiler 5 0 bis 1000
+teiler = 5
+list=[]
+i = 0
+while i <= 1000:
+    list.append(i)
+    i = i + teiler 
+print(list)
+
+# untere grenze 1 obere grenez 100 teiler 5 
+untere_grenze = 1 
+obere_grenze = 100 
+teiler = 5
+list= []
+i = untere_grenze
+while i <= obere_grenze:
+    list.append(i)
+    i = i + teiler
+print(list)
+
+#while-schleife 1 bis 50
+list1 = []
+i = 1
+while i <= 50:
+    list.append(i)
+    i = i + 1
+print(list1)
+
+# -20 nis 20
+list2 = []
+i = -20 
+while i >= 20:
+    list.append(i)
+    i = i +1 
+print(list2)
+
+#60 bis 99 durch 3 teilbar
+list3 = []
+i = 60
+while i <= 99:
+    list.append(i)
+    i = i + 6
+print(list3)
+
+# alle zahlen aus liste3 untereinander ausgeben 
 
 
 
-
-
-
+# TODO lernen auf nächste woche wegen Test
 
 
 
