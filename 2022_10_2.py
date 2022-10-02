415f6bf7b908c2c0f2ef993b84c024e3b972f6b0
kohijelly <cassie.zhong@outlook.de>
2022-10-02 11:30:09 +0200

    Create 2022_10_2.py

---
+++ 2022_10_2.py
@@ -0,0 +1,89 @@
+#wine while-schleife, die alle zhalen zwischen 1 und 100 in einer varibialen summe aufsummiert
+summe1 = 0 
+i = 1
+while i <= 100 :
+    summ1 = summe1 + i
+    i = i + 1
+print(summe1)
+
+#eine while-schleife, die alle Zahlen zwischen 50  und 150 in einer Variablen summe2 ausgibt
+summe2 = 0 
+i = 50
+while i <= 150 :
+    i = i + 1
+print(i)
+
+#eine while schleife, die alle durch 5 teilbare zahlen zwischen 25 und 255in einer variablen summe3 ausgibt
+summe3 = 0 
+i = 25
+while i <= 255 :
+    i = i + 5
+print(i)
+
+# eine schleife, die die zahlen 0 1 1 2 3 5 8  13 21 34 55 89 144 233 377 610 987 ausgibt 
+f1 = 0 
+f2 = 1
+while f1 <= 987:
+    print(f1)
+    temp = f1
+    f1 = f2
+    f2 = temp + f2
+
+
+# eine schleife, die die zahlen 0 1 4 9 16 25 36 49 64 81 100 121 144 169 196 255 ausgibt
+i = 0
+while i <= 15:
+    i = i * i
+print(1)
+
+#eine while-schleife, die die Zahlen 1 2 4 8 16 32 64  128 256 512 1024 ausgibt
+i = 1
+while i <= 1024:
+    i = i * 2
+print(i)
+
+#eine while-schleife die alle Zahlen zwischen untere_grenze und obere_grenze in einer Variablen summe4 ausgibt
+summe4 = 0 
+i = untere_grneze
+while i <= obere_grneze:
+    i = i + 1
+print(i)
+
+#eine while-schleife, die eine Liate alle zahen von 1 bis 50 erstellt
+list1 = []
+i = 1
+while i <= 50:
+    list1.append(i)
+    i = i + 1
+print(list1)
+
+#eine while-schleife die eine liste zwischen -20 und 20 erstellt
+list2 = []
+i = -20 
+while i <= 20 :
+    list2.append(i)
+    i = i + 1
+print(list2)
+
+#eine while-schleife die eine liste aller durch 3 teilbaren zahlen zwischen 60 und 99 erstellt
+list3 = []
+i = 60 
+while i <= 99 :
+    list3.append(i)
+    i = i + 3
+print(list3)
+
+#eine while-schleife die alle zahlen aus liste3 untereinander ausgibt
+#HINWEIS 
+l = [12, 5, 9]
+print(len(l))#3
+print(len[0])#12
+print(len[2])#9
+print(len[1])#5
+
+n
+
+
+
+
+
