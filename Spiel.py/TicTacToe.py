dc46787a996738a46bfcc0386e6c071137da7d94
kohijelly <105992024+kohijelly@users.noreply.github.com>
2023-05-21 11:31:51 +0200

    tictactoe

--- Spiel.py/TicTacToe.py
+++ Spiel.py/TicTacToe.py
@@ -2,7 +2,32 @@ import arcade
 
 class TicTacToe(arcade,Window):
     def__init__(self, breite, höhe, titel)
-        super()._init_(breite, höhe, titel)
+        super().__init__(breite, höhe, titel)
+    
+    def koordinaten_zu_feld(self, x, y):
+        if x < 200 and y > 400:
+            return 1 
+        if x < 400 and y > 400:
+            return 2
+        if x < 600 and y > 400:
+            return 3
+        if x < 200 and 200 < y < 400:
+            return 4
+        if 200 < x < 400 and 200 < y < 400:
+            return 5 
+        if x > 400 and 200 < y < 400:
+            return 6
+        if x < 200 and y < 200:
+            return 7
+        if 200 < x < 400 and y < 200:
+            return 8 
+        if x > 400 and y < 200:
+            return 9
+
+
+
+    def on_mous_press(self, x, y, button, modifiers):
+        feld=self.koordinaten_zu_feld(x, y)
     
     def on_draw (self):
 
